f24a0cc74b050125ffc6d43f81a98d217f118879
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-20 06:50:50 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,40 @@
+// //  1:Equality comparison (True)
+// let car: string = 'subaru';
+// console.log("Is car == 'subaru'? i pridict True.");
+// console.log(car == 'subaru'); 
+
+// // 2:Strict Equality comparison (True)
+// console.log("Is car === 'subaru'? i pridict True.");
+// console.log(car === 'subaru'); 
+
+// //  3:Inequality comparison (False)
+// console.log("Is car != 'subaru'? i pridict False.");
+// console.log(car != 'subaru'); 
+
+// // 4:Strict inequality comparison (False)
+// console.log("Is car !== 'subaru'? i pridict False.");
+// console.log(car !== 'subaru'); 
+
+// // 5:Less then comparison (False)
+// console.log("Is car < 'subaru'? i pridict False.");
+// console.log(car < 'subaru');
+
+// // 6:Greater than comparison (False)
+// console.log("Is car > 'subaru'? i pridict False.");
+// console.log(car > 'subaru');
+
+// // 7:Less than or equal comparison (True)
+// console.log("Is car <= 'subaru'? i pridict True.");
+// console.log(car <= 'subaru');
+
+// // 8:Greater than or equal comparison (True)
+// console.log("Is car >= 'subaru'? i pridict True.");
+// console.log(car >= 'subaru');
+
+// // 9:Checking truthness (True)
+// console.log("Is car? i pridict True.");
+// console.log(car);
+
+// // 10:Checking falsiness (False)
+// console.log("Is !car? i pridict False.");
+// console.log(!car);
